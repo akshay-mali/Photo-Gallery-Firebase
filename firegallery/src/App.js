cb06657bc370d0ed833596dc237186aa2c18e8e0
Akshay9080 <akshaybmali01@gmail.com>
2020-11-12 18:18:02 +0530

    Setting up react project and firebase

---
+++ firegallery/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Title from './components/Title';
+import UploadForms from './components/UploadForms';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <UploadForms />
+    </div>
+  );
+}
+
+export default App;
---
+++ firegallery/src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDa_WOdXdRedw0lqCkHFuw3iC2yoDzuaCE",
+    authDomain: "photo-gallery-68ac4.firebaseapp.com",
+    databaseURL: "https://photo-gallery-68ac4.firebaseio.com",
+    projectId: "photo-gallery-68ac4",
+    storageBucket: "photo-gallery-68ac4.appspot.com",
+    messagingSenderId: "33828938294",
+    appId: "1:33828938294:web:013a1b2de86c2f95a1ae97"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.Firestore();
+
+  export {projectFirestore, projectStorage};
